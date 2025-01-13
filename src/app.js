88024a2faa7a722691a388c4f2bb387ee3210f52
Bautista Centorbi <bautii.centorbii@gmail.com>
2025-01-13 11:41:22 -0300

    Handlebars Implementation + products mapping

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+import { router as vistasRouter } from './routes/viewsRouter.router.js';
 import { router as productsRouter} from './routes/products.router.js';
 import { router as cartsRouter } from './routes/carts.router.js';
 
@@ -6,14 +8,21 @@ const PORT=8080;
 
 const app=express();
 
+app.use(express.static('./src/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use('/api/products',productsRouter);
-app.use('/api/carts',cartsRouter);
+
+app.engine('handlebars', engine());
+app.set('views', './src/views');
+app.set('view engine', 'handlebars');
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+app.use('/', vistasRouter);
 
 app.get('/',(req,res)=>{
-    res.setHeader('Content-Type','text/plain');
-    res.status(200).send('Pre Entrega 1 - BAUTISTA CENTORBI \n CoderHouse - Backend 1 - 70425');
+
+    res.render('index', {});
 })
 
 const server=app.listen(PORT,()=>{
